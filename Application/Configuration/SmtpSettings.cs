392dc1ceadf53adabf85204dc6dc5296247e26bd
Ferreyra Enzo <ferreyraenzo26@hotmail.com>
2024-05-01 22:34:59 -0300

    Se genero la estructura para el envio de confirmacion de email

---
+++ Application/Configuration/SmtpSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Configuration
+{
+    public class SmtpSettings
+    {
+        public String Server { get; set; }
+        public int Port { get; set; }
+        public String SenderName { get; set; }
+        public String UserName { get; set; }
+        public String SenderEmail { get; set; }
+        public String Password { get; set; }
+
+
+    }
+}
